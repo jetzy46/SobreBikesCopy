5895c0a8421505cd4d3528d39e38a931e64dcd22
Johan Linol <linol.johan@gmail.com>
2022-05-04 14:56:19 +0200

    added styled-component, started the structure of the project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Sobre Bikes Copy
+# Sobre Bikes Copy
+
+Copy of the website of Sobre bikes
+Seems easy to do, and with minors difficulties
---
+++ src/App/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <a href="/">
+            <img src="" alt="" />
+          </a>
+          <a href="/">
+            <img src="" alt="" />
+          </a>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
