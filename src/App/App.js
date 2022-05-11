17548ba75c14e009c11b2fdca29592502335b10b
Johan Linol <linol.johan@gmail.com>
2022-05-11 11:14:17 +0200

    added react-router

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
+// eslint-disable-next-line
 import { Header, Frames, Brand, Footer } from "./index";
+// eslint-disable-next-line
 import { Dad, Off, Versatile, Multi } from "./index";
 
 function App() {
